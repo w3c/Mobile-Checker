76c4d415eae550102966f7b994558a5b020d5e1e
Dominique Hazael-Massieux <dom@w3.org>
2014-08-21 13:57:33 +0200

    allowing to run test server with different static files path

--- test/test_server/test_app.js
+++ test/test_server/test_app.js
@@ -9,7 +9,6 @@ app.use(compress({
   threshold: 512
 }));
 
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res){
     res.sendfile('index.html');
@@ -18,7 +17,12 @@ app.get('/redirect.css', function (req, res) {
     res.redirect('/css/style.css');
 });
 
-exports.start = function (port) {
+exports.start = function (port, path) {
+    path = path || '/public';
+    path =  __dirname + path;
+    console.log(path);
+    app.use(express.static(path));
+
     port = port || 3001;
     http.listen(port, function(){
         console.log('listening on *:' + port);
