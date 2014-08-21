9f18875efba31285318326acf88d581f59803248
Dominique Hazael-Massieux <dom@w3.org>
2014-08-22 00:00:41 +0200

    added test for flow of browser execution with proxy

--- test/browser-emulator-tests.js
+++ test/browser-emulator-tests.js
@@ -1,6 +1,11 @@
+global.rootRequire = function(name) {
+    return require(__dirname + '/../' + name);
+}
+
 var Browser = require("../lib/mobile-browser-emulator").Browser
 ,   expect = require("expect.js")
 ,   webdriver = require('selenium-webdriver');
+
 
 
 describe("Starting and quiting browser", function () {
@@ -73,6 +78,33 @@ describe("Getting data from network", function () {
             done();
         });
         browser.open("http://localhost:3001/ok.html");
+    });
+
+    after(function () {
+        server.close();
+    });
+});
+
+describe("Getting data from browser and network", function () {
+    var server = require("./test_server/test_app.js");
+    var browser = new Browser({browsermobProxy: {port:8080}, trackNetwork: true});
+    before(function () {
+        server.start(3001, '/../browser-tests');
+    });
+
+    it("should get the status code and title of a loadable page", function (done) {
+        browser.on('har', function (har) {
+            expect(har.log.entries[0].response.status).to.be(200);
+            done();
+        });
+        browser.open("http://localhost:3001/ok.html");
+        browser.do(function () {
+            return driver.findElement(webdriver.By.tagName('title')).then(function (title) {
+                title.getInnerHtml().then(function (titleText) {
+                    expect(titleText).to.be('OK');
+                });
+            });
+        });
     });
 
     after(function () {
