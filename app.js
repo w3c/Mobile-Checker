56e91d5fd7c90879853099808267380edd67d044
Dominique Hazael-Massieux <dom@w3.org>
2014-08-26 23:12:48 +0200

    js-beautified

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ global.rootRequire = function(name) {
 }
 
 var express = require("express"),
-	app = express(),
-	http = require('http').Server(app),
-	io = require('socket.io')(http),
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
     util = require("util"),
     Checker = require("./lib/checker").Checker,
     events = require("events"),
-    logger  = require('morgan'),
+    logger = require('morgan'),
     uuid = require('node-uuid'),
     proc = require('child_process');
 
@@ -17,85 +17,84 @@ var express = require("express"),
 var fs = require("fs");
 var step;
 var checklist = [
-    require('./lib/checks/performance/number-requests')
-,   require('./lib/checks/performance/redirects')
-,   require('./lib/checks/performance/http-errors')
-,   require('./lib/checks/performance/compression')
-,   require('./lib/checks/responsive/doc-width')
-,   require('./lib/checks/responsive/meta-viewport')
-,   require('./lib/checks/responsive/fonts-size')
-,   require('./lib/checks/compatibility/flash-detection')
-,   require('./lib/checks/compatibility/css-prefixes')
-,   require('./lib/checks/interactions/alert')
-,   require('./lib/checks/interactions/touchscreen-target')
+    require('./lib/checks/performance/number-requests'), require(
+        './lib/checks/performance/redirects'), require(
+        './lib/checks/performance/http-errors'), require(
+        './lib/checks/performance/compression'), require(
+        './lib/checks/responsive/doc-width'), require(
+        './lib/checks/responsive/meta-viewport'), require(
+        './lib/checks/responsive/fonts-size'), require(
+        './lib/checks/compatibility/flash-detection'), require(
+        './lib/checks/compatibility/css-prefixes'), require(
+        './lib/checks/interactions/alert'), require(
+        './lib/checks/interactions/touchscreen-target')
 ];
 
 app.use(logger());
 app.use(express.static("public"));
 
-function Sink () {}
+function Sink() {}
 util.inherits(Sink, events.EventEmitter);
 
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
     var address = socket.handshake.address;
-    socket.on('check', function(data){
-        var sink = new Sink
-        ,   checker = new Checker
-        ,   uid = uuid.v4()
-        ,   screenshot = false
-        ;
-        sink.on('ok', function(data){
+    socket.on('check', function(data) {
+        var sink = new Sink,
+            checker = new Checker,
+            uid = uuid.v4(),
+            screenshot = false;
+        sink.on('ok', function(data) {
             socket.emit('ok', data);
         });
-        sink.on('warning', function(data){
+        sink.on('warning', function(data) {
             socket.emit('warning', data);
         });
-        sink.on('err', function(data){
+        sink.on('err', function(data) {
             socket.emit('err', data);
         });
-        sink.on('screenshot', function(data){
+        sink.on('screenshot', function(data) {
             screenshot = true;
             socket.emit('screenshot', data);
         });
-        sink.on('done', function(){
+        sink.on('done', function() {
             step++;
             console.log('done');
             socket.emit('done', step);
         });
-        sink.on('end', function(data){
+        sink.on('end', function(data) {
             socket.emit('end', data);
         });
-        sink.on('exception', function(data){
+        sink.on('exception', function(data) {
             socket.emit('exception', data);
         });
-        socket.on('disconnect', function () {
+        socket.on('disconnect', function() {
             io.sockets.emit('user disconnected');
-            if(screenshot){
-                fs.unlink('public/'+uid+'.png', function (err) {
-                if (err) throw err;
+            if (screenshot) {
+                fs.unlink('public/' + uid + '.png', function(err) {
+                    if (err) throw err;
                     console.log('delete with success');
-                });   
+                });
             }
         });
         socket.emit('start', 3);
         checker.check({
-            url : data.url
-        ,   events : sink
-        ,   sockets : socket
-        ,   profile : data.profile
-        ,   checklist : checklist
-        ,   id : uid
-        ,   lang : "en"
+            url: data.url,
+            events: sink,
+            sockets: socket,
+            profile: data.profile,
+            checklist: checklist,
+            id: uid,
+            lang: "en"
         });
         step = 0;
-    }); 
+    });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function() {
+    console.log('listening on *:3000');
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,60 +1,65 @@
 var socket = io();
 
 var settings = {
-	widthView : 600,
-	heightView : 900, 
-	profile : null,
-	url : null
+    widthView: 600,
+    heightView: 900,
+    profile: null,
+    url: null
 };
 var progressBar = {
-	total : 0,
-	done : 0,
-	status : 0
+    total: 0,
+    done: 0,
+    status: 0
 };
 
 var result = {};
 var smartphoneSelectorHidden = 1;
 var checkSelectorHidden = 1;
 /*
-*	functions
-*/
-function progress () {
-	progressBar.status = (progressBar.done/progressBar.total)*100;
-	//$('.progress-bar').attr("aria-valuenow", progressBar.status);
-	//$('.progress-bar').attr("style", "width:" + progressBar.status + "%");
+ *	functions
+ */
+function progress() {
+    progressBar.status = (progressBar.done / progressBar.total) * 100;
+    //$('.progress-bar').attr("aria-valuenow", progressBar.status);
+    //$('.progress-bar').attr("style", "width:" + progressBar.status + "%");
 }
 
-function stringifySourceCode () {
-	result.source = result.source.replace('<', '&lt;');
-	result.source = result.source.replace('>', '&gt;');
+function stringifySourceCode() {
+    result.source = result.source.replace('<', '&lt;');
+    result.source = result.source.replace('>', '&gt;');
 }
 
-function loadHomePage () {
+function loadHomePage() {
     $('#sidebar').hide();
-	$('#report').removeClass('report');
-	$('#home').removeClass('report');
+    $('#report').removeClass('report');
+    $('#home').removeClass('report');
     $('#sm').show();
     $('#sm').removeClass('screenshot');
     $('#console-title').hide();
-	$('#console').hide();
+    $('#console').hide();
 }
 
-function loadProgressPage () {
-    var scales = {'sm':8.31, 'sm2':8.31, 'tab':2.5};
-    var selectedDevice = $('input[name=device]:checked').parent().find('img').eq(0);
+function loadProgressPage() {
+    var scales = {
+        'sm': 8.31,
+        'sm2': 8.31,
+        'tab': 2.5
+    };
+    var selectedDevice = $('input[name=device]:checked').parent().find('img').eq(
+        0);
     var id = selectedDevice.attr('id');
     var scale = scales[id];;
     var offset2 = selectedDevice.offset();
     var offset1 = $('#smartphone').offset();
     var transform = 'transform: translate3d(' +
-      (offset1.left - offset2.left) + 'px, ' +  (offset1.top - offset2.top)
-      + 'px,0) scale(' + scale + ',' + scale + ') rotate(360deg)';
-    var style = $('<style>#' + id + '.screenshot {  ' +  transform
-                  + ' }</style>').appendTo('head');
+        (offset1.left - offset2.left) + 'px, ' + (offset1.top - offset2.top) +
+        'px,0) scale(' + scale + ',' + scale + ') rotate(360deg)';
+    var style = $('<style>#' + id + '.screenshot {  ' + transform +
+        ' }</style>').appendTo('head');
     selectedDevice.addClass('screenshot');
     $('#home').addClass('report');
     $('#report').addClass('report');
-    setTimeout(function () {
+    setTimeout(function() {
         $('#smartphone').addClass(id);
         selectedDevice.hide();
         selectedDevice.removeClass('screenshot');
@@ -62,52 +67,51 @@ function loadProgressPage () {
     }, 1000)
 }
 
-function loadResultPage () {
-}
+function loadResultPage() {}
 
 /*
-*	protocol
-*/
+ *	protocol
+ */
 
-$('.device-selector').click(function (){
-	var id = this.id;
-	settings.profile = id;
-	$('#device-selected').text(id);
+$('.device-selector').click(function() {
+    var id = this.id;
+    settings.profile = id;
+    $('#device-selected').text(id);
 });
 
 loadHomePage();
 
-socket.on('start', function (data){
-	//progressBar.total = data;
-	loadProgressPage();
+socket.on('start', function(data) {
+    //progressBar.total = data;
+    loadProgressPage();
 });
-socket.on('done', function (data){
-	progressBar.done++;
-	//progress();
+socket.on('done', function(data) {
+    progressBar.done++;
+    //progress();
 });
-socket.on('ok', function (data){
+socket.on('ok', function(data) {});
+socket.on('err', function(data) {
+    $('#issues-feed').append($(data));
 });
-socket.on('err', function (data){
-	$('#issues-feed').append($(data));
+socket.on('screenshot', function(path) {
+    $('<img>').attr('src', path).attr('alt', 'Screenshot').attr('id',
+        'screenshot').attr('width', 266).appendTo('#smartphone');
 });
-socket.on('screenshot', function (path){
-        $('<img>').attr('src', path).attr('alt', 'Screenshot').attr('id','screenshot').attr('width',266).appendTo('#smartphone');
+socket.on('end', function() {
+    loadResultPage();
 });
-socket.on('end', function (){
-	loadResultPage();
-});
-socket.on('exception', function (msg){
-	$('#console-title').show();
-	$('#console').show();
-	$('#console').text(msg);
+socket.on('exception', function(msg) {
+    $('#console-title').show();
+    $('#console').show();
+    $('#console').text(msg);
 });
 
-$('form').submit(function (){
-	settings.url = $('#url').val();
-	settings.profile = $('input[name="device"]:radio:checked').val();
+$('form').submit(function() {
+    settings.url = $('#url').val();
+    settings.profile = $('input[name="device"]:radio:checked').val();
     $('#url').val('');
-	socket.emit('check', settings);
-	return false;
+    socket.emit('check', settings);
+    return false;
 });
 
 $('span.info').popover();
