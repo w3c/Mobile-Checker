1c143cff4323a067009cca37b920f81acc7d8f2f
Dominique Hazael-Massieux <dom@w3.org>
2015-06-30 15:34:09 -0400

    Use uniform endpoint for websockets close #103

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,5 @@
 // webSockets client side declaration
-if(window.location.host == "localhost:3000")
-    var socket = io.connect();
-else
-    var socket = io.connect({ path: '/mobile-alpha/socket.io' });
+var socket = io.connect({ path: '/mobile-alpha/socket.io' });
 
 //settings sent to server
 // profile : device profile, selected by user.
