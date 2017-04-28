756fe94187523f3c5ca776afaf0f0212fc79acb2
QwertyChouskie <qwertychouskie@users.noreply.github.com>
2017-04-28 16:52:35 -0700

    Update .travis-setup.sh

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -24,6 +24,7 @@ else
 fi
 
 # Download a custom chrome-sandbox which works inside OpenVC containers (used on travis).
+# This is not needed in Trusty containers, see https://github.com/travis-ci/travis-ci/issues/938#issuecomment-273915626.
 #sudo rm -f $CHROME_SANDBOX
 #sudo wget https://googledrive.com/host/0B5VlNZ_Rvdw6NTJoZDBSVy1ZdkE -O $CHROME_SANDBOX
 #sudo chown root:root $CHROME_SANDBOX; sudo chmod 4755 $CHROME_SANDBOX
